c039989e74f7b196db85d66b614cb4145189eb96
sri-rishi <sririshi2612@gmail.com>
2024-02-02 22:55:32 +0530

    fix: fix input box value

--- src/components/errorBox/ErrorBox.jsx
+++ src/components/errorBox/ErrorBox.jsx
@@ -1,10 +1,6 @@
-import { useNavigate } from "react-router-dom";
 import { SCARECROW } from "../../assets"
 
 const ErrorBox = () => {
-    const navigate = useNavigate();
-
-
     return (
         <div>
             <p className="not-found">404 NOT FOUND</p>
@@ -13,7 +9,6 @@ const ErrorBox = () => {
                 <div className="text-section">
                     <h1>I have bad news for you</h1>
                     <p>The City you entered is not available</p>
-                    <button className="error-btn" onClick={() => navigate("/")}>BACK TO HOMEPAGE</button>
                 </div> 
             </div>
         </div>
--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -4,18 +4,23 @@ import { getCity} from "../../slices/weatherDataSlice";
 
 
 const SearchBox = () => {
-    const [newCity, setNewCity] = useState();
+    const [newCity, setNewCity] = useState("");
     const dispatch = useDispatch();
 
     const handleSearchClick = () => {
         dispatch(getCity(newCity));
+        setNewCity("")
     }
     return (
         <div className="search-box">
-            <input onChange={(e) => setNewCity(e.target.value)} className="input-box" placeholder="Enter city name"/>
+            <input
+                value={newCity}
+                onChange={(e) => {setNewCity(e.target.value)}}
+                className="input-box" placeholder="Enter city name"
+            />
             <button
                 className="search-btn"
-                onClick={() => handleSearchClick()}
+                onClick={() => {handleSearchClick()}}
             >
                 Check
             </button>
