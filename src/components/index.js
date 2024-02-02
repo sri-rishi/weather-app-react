4e1395adb85dd594dedf941d4c85936d5b181daf
sri-rishi <sririshi2612@gmail.com>
2024-02-02 22:42:02 +0530

    import and export files

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from "./navbar/Navbar";
+import SearchBox from "./searchBox/SearchBox";
+import CurrentWeather from "./currentWeather/CurrentWeather";
+import Forecast from "./fiveDayforecast/Forecast";
+import CurrentWeatherDetails from "./currentWeatherDetails/CurrentWeatherDetails";
+import ForeCastList from "./fiveForecastList/ForecastList";
+import ErrorBox from "./errorBox/ErrorBox";
+
+export {
+    Navbar,
+    SearchBox,
+    CurrentWeather,
+    CurrentWeatherDetails,
+    Forecast,
+    ForeCastList,
+    ErrorBox
+}
