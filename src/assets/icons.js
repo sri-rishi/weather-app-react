6ac4b11fa7106ebca4fc8cdf96f8db7d4b66a2a2
sri-rishi <sririshi2612@gmail.com>
2024-02-02 22:35:53 +0530

    style: import icons and images

---
+++ src/assets/icons.js
@@ -0,0 +1,8 @@
+import { TiWeatherSnow } from "react-icons/ti";
+import { RiDropFill } from "react-icons/ri";
+import { FiWind } from "react-icons/fi";
+
+
+
+
+export {TiWeatherSnow, RiDropFill, FiWind}
---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import {TiWeatherSnow, RiDropFill, FiWind} from "./icons";
+import SCARECROW from "./images/Scarecrow.png";
+
+export {TiWeatherSnow, RiDropFill, FiWind, SCARECROW}
