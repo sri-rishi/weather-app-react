14dfe870af279bb5c4ac36239364c62a32bbba0b
sri-rishi <sririshi2612@gmail.com>
2024-02-02 22:42:53 +0530

    feat: add allroute component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { ErrorBox,Navbar } from './components';
+import { useEffect } from 'react';
+import { fiveDayForecast, weatherData } from './slices/weatherDataSlice';
+import Home from './pages/Home';
+import { AllRoutes } from './router/AllRoute';
+
+const API_KEY =  process.env.REACT_APP_WEATHER_API_KEY
 
 function App() {
+  const {city, unit, error} = useSelector(store => store?.weather);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(weatherData({city, unit, API_KEY}));
+    dispatch(fiveDayForecast({city, unit, API_KEY}));
+  }, [city, unit]);
+
+
   return (
     <div className="App">
-      Hello 
+      <Navbar />
+      <AllRoutes/>
     </div>
   );
 }
